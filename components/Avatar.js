0e1b12e6a765e81aaf740385ce0c5815df780154
Namkat Cedrick <namkatcedrickjumtock3@gmail.com>
2022-08-20 18:31:18 +0100

    updates on linting rules

--- components/Avatar.js
+++ components/Avatar.js
@@ -6,6 +6,7 @@ const Avatar = ({ url, className }) => {
       className={` h-10 rounded-full cursor-pointer  transition duration-150 transform hover:scale-110 ${className}`}
       loading="lazy"
       src={url}
+      alt={'avatar'}
     />
   );
 };
